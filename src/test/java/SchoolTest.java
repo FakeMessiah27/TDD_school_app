b5aa6256c24edcc39f13935d6ec1f04775a43a17
FakeMessiah27 <thomas.vaniersel@hotmail.com>
2018-11-27 14:34:08 +0100

    Created SchoolObjectHasOpeningDate test.

--- src/test/java/SchoolTest.java
+++ src/test/java/SchoolTest.java
@@ -1,10 +1,23 @@
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class SchoolTest {
 
     private static String SCHOOL_NAME = "TestSchool";
+    private static Date openingDate;
+
+    static {
+        try {
+            openingDate = new SimpleDateFormat("dd-MM-yyyy").parse("01-09-2018");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void SchoolObjectHasAName() {
@@ -17,5 +30,17 @@ public class SchoolTest {
 
         // assert
         assertEquals(schoolName, expectedSchoolName);
+    }
+
+    @Test
+    public void SchoolObjectHasOpeningDate() {
+        // arrange
+        School school = new School(SCHOOL_NAME, openingDate);
+
+        // act
+
+
+        // assert
+        assertFalse(school.getOpeningDate() == null);
     }
 }
