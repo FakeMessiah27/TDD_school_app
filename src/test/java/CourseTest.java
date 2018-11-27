17b6063519831b12fd554cda2f4882edfd449ab9
FakeMessiah27 <thomas.vaniersel@hotmail.com>
2018-11-27 13:29:41 +0100

    Created endDateBeforeBeginDateThrowsException test.

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,5 +1,34 @@
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class CourseTest {
 
+    private static String COURSE_NAME = "TestCourse";
+
+    @Test
+    public void endDateBeforeBeginDateThrowsException() {
+        // arrange
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date beginDate = new Date();
+        Date endDate = new Date();
+        try {
+            beginDate = dateFormat.parse("28-11-2018");
+            endDate = dateFormat.parse("27-11-2018");
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // act
+        Course course = new Course(COURSE_NAME, beginDate, endDate);
+
+        // assert
+        assertTrue(course.getEndDate().after(course.getStartDate()));
+    }
 }
