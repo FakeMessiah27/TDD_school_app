a9494a149b76171117af7528b93dca5d7c76fd45
FakeMessiah27 <thomas.vaniersel@hotmail.com>
2018-11-27 14:24:39 +0100

    Created SchoolObjectHasName test.

--- src/test/java/SchoolTest.java
+++ src/test/java/SchoolTest.java
@@ -1,5 +1,21 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SchoolTest {
 
+    private static String SCHOOL_NAME = "TestSchool";
+
+    @Test
+    public void SchoolObjectHasAName() {
+        // arrange
+        School school = new School(SCHOOL_NAME);
+        String expectedSchoolName = "TestSchool";
+
+        // act
+        String schoolName = school.getName();
+
+        // assert
+        assertEquals(schoolName, expectedSchoolName);
+    }
 }
